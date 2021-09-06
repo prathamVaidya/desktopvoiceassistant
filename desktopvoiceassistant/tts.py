78880379a393b69b3f7854dff7ab7bf1eb3b78be
Pratham Vaidya <iamprathamvaidya@gmail.com>
2021-09-07 03:31:30 +0530

    Implemented tts using gTTS

---
+++ desktopvoiceassistant/tts.py
@@ -0,0 +1,25 @@
+import os
+import playsound
+
+# Constants
+AUDIO_FILE = "temp.wav"
+GOOGLE_TTS = 1
+MOZILLA_TTS = 2
+
+
+def say(text, speed=False, tts_name=GOOGLE_TTS):
+    if tts_name == GOOGLE_TTS:
+        # Use gTTS for Text to Speech. Internet Required.
+        from gtts import gTTS
+
+        tts = gTTS(text, slow=speed)
+        tts.save(AUDIO_FILE)
+        playsound.playsound(AUDIO_FILE, True)
+        os.system("rm " + AUDIO_FILE)
+
+    elif tts_name == MOZILLA_TTS:
+        # Use Mozilla TTS with Tacotron-2
+
+        # Todo: Implement this
+        print("Not Available")
+
