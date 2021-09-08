c5f719b277ba0cddd5b8c75a0c49213a2529d8aa
Jitesh <decifur07@gmail.com>
2021-09-08 10:31:56 +0530

    minor changes

--- desktopvoiceassistant/stt.py
+++ desktopvoiceassistant/stt.py
@@ -2,15 +2,13 @@ import speech_recognition as sr
 
 
 # todo::
-# - change function name to listen, call is misleading to other programmer.
 # - use this wake_world variable for wake world.
-# try not to use Hello Nova because when using other commands it will be very long
 # you can use hello nova as greeting msg and just return a command "hello" which is handled in commands.py
 
 wake_word = "nova"
 
 
-def call():   # It takes microphone input from the user and returns string output
+def listen():   # It takes microphone input from the user and returns string output
     listener = sr.Recognizer()
     try:
         with sr.Microphone() as source:
@@ -18,20 +16,19 @@ def call():   # It takes microphone input from the user and returns string outpu
             voice = listener.listen(source)
             command = listener.recognize_google(voice)  # Using google for voice recognition.
             arr = command.lower().split()
-            if 'hello' == arr[0] and 'nova' == arr[1]:  # The command should start with "Hello Nova"...
-                arr.pop(0)  # removes first word HELLO
-                arr.pop(0)  # removes second word NOVA
+            if 'nova' == arr[0]:  # The command should start with "Hello Nova"...
+                arr.pop(0)  # removes first word NOVA
                 print(" ".join(arr))
             elif 'thank you' in command:
-                return print('okay bye!')
+                return print('Okay Bye!')
             else:
-                call()
+                listen()
 
     except Exception as error:
         # Handle exceptions
         print('Exception -> ' + str(error))
         print('Please speak again...')  # This will be printed in case of improper voice or some mic issue
-        call()
+        listen()
         pass
 
 
