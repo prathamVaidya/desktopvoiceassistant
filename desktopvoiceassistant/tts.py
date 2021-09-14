da9b07469833374f9167dbfd8e0c6dba5e3470ef
Jitesh <decifur07@gmail.com>
2021-09-14 23:35:07 +0530

    playSound error solved

--- desktopvoiceassistant/tts.py
+++ desktopvoiceassistant/tts.py
@@ -1,26 +1,28 @@
 import os
-import playsound
+from playsound2 import playsound
+
+
 
 # Constants
-AUDIO_FILE = "temp.wav"
+AUDIO_FILE = "temp.mp3"
 GOOGLE_TTS = 1
 MOZILLA_TTS = 2
 
 
 def say(text, speed=False, tts_name=GOOGLE_TTS):
     print(text)
+
     if tts_name == GOOGLE_TTS:
         # Use gTTS for Text to Speech. Internet Required.
         from gtts import gTTS
 
         tts = gTTS(text, slow=speed)
         tts.save(AUDIO_FILE)
-        playsound.playsound(AUDIO_FILE, True)
-        os.system("rm " + AUDIO_FILE)
+        playsound(AUDIO_FILE, True)
+        os.remove(AUDIO_FILE)
 
     elif tts_name == MOZILLA_TTS:
         # Use Mozilla TTS with Tacotron-2
 
         # Todo: Implement this
         print("Not Available")
-
